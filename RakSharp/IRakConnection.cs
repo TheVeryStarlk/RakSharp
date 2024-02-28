bdd3937785cae1bab667758d6feccc6ecd919d96
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2024-02-29 01:26:39 +0300

    Write initial API code

---
+++ RakSharp/IRakConnection.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+
+namespace RakSharp;
+
+/// <summary>
+/// Represents a RakNet connection to an <see cref="IPEndPoint"/>.
+/// </summary>
+public interface IRakConnection
+{
+    /// <summary>
+    /// The <see cref="IPEndPoint"/> which the connection is connected to.
+    /// </summary>
+    public IPEndPoint RemoteEndPoint { get; }
+
+    /// <summary>
+    /// The maximum amount of bytes that can be transmitted across the network.
+    /// </summary>
+    public short MaximumTransmissionUnit { get; }
+
+    /// <summary>
+    /// Writes data to the connected <see cref="IPEndPoint"/>.
+    /// </summary>
+    /// <param name="memory">The data to send.</param>
+    /// <param name="token">The token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="Task"/> that represents the asynchronous write operation.</returns>
+    public Task WriteAsync(Memory<byte> memory, CancellationToken token = default);
+
+    /// <summary>
+    /// Reads data from the connected <see cref="IPEndPoint"/>.
+    /// </summary>
+    /// <param name="token">The token to monitor for cancellation requests.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous read operation,
+    /// which wraps the <see cref="Memory{T}"/> containing the contents of data.
+    /// </returns>
+    public Task<Memory<byte>> ReadAsync(CancellationToken token = default);
+
+    /// <summary>
+    /// Disconnects the connection and stops all operations.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> that represents the asynchronous disconnect operation.</returns>
+    public Task DisconnectAsync();
+}
---
+++ RakSharp/RakConnection.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+
+namespace RakSharp;
+
+/// <summary>
+/// Stores options that control the <see cref="RakConnection"/>.
+/// </summary>
+public sealed class RakConnectionOptions
+{
+    /// <summary>
+    /// The <see cref="IPEndPoint"/> to connect to.
+    /// </summary>
+    public required IPEndPoint RemoteEndPoint { get; set; }
+
+    /// <summary>
+    /// Specifies for how long the <see cref="RakConnection"/> should attempt to connect before it times out.
+    /// </summary>
+    public required TimeSpan TimeOut { get; set; }
+
+    /// <summary>
+    /// The maximum amount of bytes that can be transmitted across the network.
+    /// </summary>
+    public required short MaximumTransmissionUnit { get; set; }
+}
+
+public sealed class RakConnection : IRakConnection
+{
+    public IPEndPoint RemoteEndPoint => throw new NotImplementedException();
+
+    public short MaximumTransmissionUnit => throw new NotImplementedException();
+
+    /// <summary>
+    /// Attempts to initiate a <see cref="RakConnection"/>.
+    /// </summary>
+    /// <param name="options">The provided <see cref="RakConnectionOptions"/> to control the <see cref="RakConnection"/>.</param>
+    /// <returns>
+    /// A <see cref="Task"/> that represents the asynchronous connect operation,
+    /// which wraps the initiated <see cref="RakConnection"/>.
+    /// </returns>
+    /// <exception cref="NotImplementedException">The method is not implemented yet.</exception>
+    public static Task<IRakConnection> ConnectAsync(RakConnectionOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task WriteAsync(Memory<byte> memory, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Memory<byte>> ReadAsync(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisconnectAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
